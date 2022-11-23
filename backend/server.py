f9a2ecf210eaf4e0cc2d8985552abdb27d51c12e
Jack <jack.m.dowling26@gmail.com>
2022-11-23 13:54:05 -0700

    created react app and trimmed fat

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import datetime
+  
+timeNow = datetime.datetime.now()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Homepage'
+
+@app.route('/data')
+def get_time():
+    return {
+        'Name':"Jack", 
+        "Age":"33",
+        "Date":timeNow, 
+        "programming":"python and react!"
+        }
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+
+	const [data, setData] = useState({
+		name: "",
+		age: 0,
+		date: "",
+		programming: "",
+	});
+
+	useEffect(() => {
+		fetch("/data").then((res) =>
+			res.json().then((data) => {
+				setData({
+					name: data.Name,
+					age: data.Age,
+					date: data.Date,
+					programming: data.programming,
+				});
+			})
+		);
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>React and flask</h1>
+				{/* Calling a data from setdata for showing */}
+				<p>{data.name}</p>
+				<p>{data.age}</p>
+				<p>{data.date}</p>
+				<p>{data.programming}</p>
+
+			</header>
+		</div>
+	);
+}
+
+export default App;
